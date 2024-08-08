c138d8f9c50980b5d410196dbc95ec0657fa5f2e
NicholaChen <nicholachen08@gmail.com>
2024-08-08 18:40:22 +0800

    generated map map is colored with red being a bomb still bugs w/ calculating number of bombs adjacent

--- index.js
+++ index.js
@@ -3,6 +3,73 @@ const ctx = canvas.getContext("2d");
 
 const margin = 0.03;
 
+const size = 10.; // number on side
+
+var map = []; // -1 represents a bomb
+
+
+for (let i = 0; i < size; i++) {
+    map[i] = [];
+    for (let j = 0; j < size; j++) {
+        map[i][j] = {value: NaN, opened: false, flagged: false};
+    }
+}
+
+function generate(bombs, firstx, firsty) {
+    for (let i=0;i<bombs;i++) {
+        let x= Math.floor(Math.random() * size);
+
+        if (x == firstx) { // stops immediately hitting a bomb
+            let y= Math.floor(Math.random() * (size - 1));
+
+            let a = []
+            for (let j=0;j<size;j++) {
+                if (j != firsty) {
+                    a.push(j);
+                }
+            }
+
+            console.log(x, a[y]);
+
+            map[a[y]][x].value = -1;
+        }
+        else {
+            let y = Math.floor(Math.random() * size);
+
+            console.log(x,y);
+
+            map[y][x].value = -1;
+        }
+    }
+
+    for (let x=0;x<size;x++) {
+        for (let y=0;y<size;y++) {
+            if (map[y][x].value != -1) {
+                map[y][x].value = adjacentBombs(x,y);
+            }
+        }
+    }
+}
+
+function adjacentBombs(x,y) {
+    let b = 0;
+    for (let i=-1;i<=1;i++) {
+        for (let j=-1;j<=1;j++) {
+            if (i != 0 && j != 0) {
+                if (x+i >= 0 && x+i < size && y+j >= 0 && y+j < size) {
+                    if (map[y+j][x+i].value == -1) {
+                        b += 1;
+                    }
+                }
+            }
+        }
+    }
+
+    return b;
+}
+generate(18,0,0);
+console.log(map);
+
 function draw(clear=false) {
     if (clear) {
         ctx.clearRect(0,0,canvas.width,canvas.height);
@@ -10,14 +77,22 @@ function draw(clear=false) {
     ctx.fillStyle = "rgba(120,120,120,0.5)";
     let w=canvas.width;
     let h=canvas.height;
-    let numSquares = 10.; // number on side
-    let squareSize = Math.min(w,h) / numSquares;
-    let startx = w/2 - squareSize * numSquares / 2;
-    let starty = h/2 - squareSize * numSquares / 2;
+    
+    let squareSize = Math.min(w,h) / size;
+    let startx = w/2 - squareSize * size / 2;
+    let starty = h/2 - squareSize * size / 2;
 
 
-    for (let x=0;x<numSquares;x++) {
-        for (let y=0;y<numSquares;y++) {
+    for (let x=0;x<size;x++) {
+        for (let y=0;y<size;y++) {
+            if (map[y][x].value == -1) {
+                ctx.fillStyle = "rgba(120,0,0,0.5)";
+            } else if (map[y][x].value == 1) {
+                ctx.fillStyle = "rgba(120,120,0,0.5)";
+            }  else {
+                ctx.fillStyle = "rgba(120,120,120,0.5)";
+            }
+
             ctx.fillRect(startx+x*squareSize + squareSize*margin,starty+y*squareSize + squareSize*margin,squareSize - squareSize*margin,squareSize - squareSize*margin);
         }
     }
@@ -55,6 +130,8 @@ function resize(entries) {
     canvas.height = displayHeight;
 
     draw();
+
+    console.log("a")
 }
 
 
